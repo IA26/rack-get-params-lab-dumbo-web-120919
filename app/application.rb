77bbcfc4d3cafd2da220fbb46803b3f31b08f048
Isaac Avilez <isaac1180@aol.com>
2020-01-28 17:33:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -17,7 +17,8 @@ class Application
       resp.write handle_search(search_term)
       
     elsif req.path.match(/cart/)
-      @@cart.each do |cart_item|
+      if @@cart
+        @@cart.each do |cart_item|
         resp.write "#{cart_item}\n"
       end 
       
